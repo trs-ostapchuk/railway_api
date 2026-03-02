4b4a42de54c8f0d86e515b88326d6919cafa8780
Taras <tarasostapchuk27@gmail.com>
2026-03-02 18:32:21 +0100

    implement Country and City models

--- stations/models.py
+++ stations/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE,
+        related_name="cities"
+    )
+
+    class Meta:
+        unique_together = ("name", "country")
+
+    def __str__(self):
+        return f"{self.name} - {self.country.name}"

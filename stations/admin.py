d1fd83e5714a9a9236cce2587ed01be804b59e16
Taras <tarasostapchuk27@gmail.com>
2026-03-02 19:20:26 +0100

    register Country, City, Station and Route in admin

--- stations/admin.py
+++ stations/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import (
+    Country,
+    City,
+    Station,
+    Route
+)
 
-# Register your models here.
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Station)
+class StationAdmin(admin.ModelAdmin):
+    list_display = ("name", "city")
+    search_fields = ("name", "city")
+
+
+@admin.register(Route)
+class RouteAdmin(admin.ModelAdmin):
+    list_display = ("source", "destination", "distance")
+    search_fields = ("source__name", "destination__name")

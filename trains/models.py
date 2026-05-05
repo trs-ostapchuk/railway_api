427b0ef165e5fc6110ea7fdd9f96a4114c4a277f
Taras <tarasostapchuk27@gmail.com>
2026-05-05 11:50:51 +0200

    add models Crew, TrainType and Train to trains and migrate

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class TrainType(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        help_text="Type of train (e.g., Intercity, Regional)."
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=100)
+    cargo_num = models.PositiveIntegerField()
+    places_in_cargo = models.PositiveIntegerField()
+    train_type = models.ForeignKey(
+        TrainType,
+        on_delete=models.CASCADE,
+        related_name="trains"
+    )
+
+    def __str__(self):
+        return self.name

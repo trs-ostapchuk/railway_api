5d6bfacfba66cbde31a063405361ce66ab9b0a16
Taras <tarasostapchuk27@gmail.com>
2026-05-05 12:01:15 +0200

    add trains viewsets

--- trains/views.py
+++ trains/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Crew,
+    TrainType,
+    Train
+)
+from .serializers import (
+    CrewSerializer,
+    TrainTypeSerializer,
+    TrainSerializer
+)
 
-# Create your views here.
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class TrainTypeViewSet(viewsets.ModelViewSet):
+    queryset = TrainType.objects.all()
+    serializer_class = TrainTypeSerializer
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer

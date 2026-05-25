caf41be4dfd7474312539a700adb22c7e4b6de86
Taras <tarasostapchuk27@gmail.com>
2026-05-25 11:00:27 +0200

    Add journey viewsets

--- journeys/views.py
+++ journeys/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Journey
+from .serializers import JourneySerializers
 
-# Create your views here.
+
+class JourneyViewSet(viewsets.ModelViewSet):
+    queryset = Journey.objects.all()
+    serializer_class = JourneySerializers
